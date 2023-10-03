678dd40707975a09876b6f41b4d5e83609ebd94d
Pablo Sánchez <pabloosancheez0@gmail.com>
2023-10-03 07:26:49 -0600

    Add template

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io ios::sync_with_stdio(0); cin.tie(0);
+#define forx(i,x,n) for (int i = x; i<n; i++)
+#define forn(i,n) for (int i = 0; i<n; i++)
+#define fi first
+#define se second
+#define pb push_back
+typedef vector<int> vi;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    fast_io;
+    int t; cin>>t;
+    while(t--)
+        solve();
+
+    return 0;
+}
